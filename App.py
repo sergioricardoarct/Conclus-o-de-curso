2e7d3e1cb2ee21325bcd990e66e0e9af13c6e291
Sergio Ricardo <sergioricardo.arct@gmail.com>
2022-10-21 15:25:33 -0300

    21-10-2022

--- App.py
+++ App.py
@@ -3,25 +3,37 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
 def homepage():
-    return render_template("homepage.html")
+    return render_template('homepage.html')
 
 @app.route('/gerente')
 def gerente():
-    return render_template("gerente.html")
+    return render_template('gerente.html')
 
 @app.route('/Administrador')
 def administrador():
-    return ("ADM")
+    return ('ADM')
 
-@app.route('/filme')
+@app.route("/filme")
 def filme():
-    return ("gerente")
+    return ("filme")
+
+@app.route('/cadstro')
+def cadastro():
+    return render_template("cadastro.html")
 
 @app.route('/login')
 def login():
-    return render_template("login.html")
+    return render_template('login.html')
+
+@app.route('/comprarIngresso')
+def comprar():
+    return render_template('compradeIngresso.html')
+
+
+
+
 
 # colocar no ar
 if (__name__ == "__main__"):
